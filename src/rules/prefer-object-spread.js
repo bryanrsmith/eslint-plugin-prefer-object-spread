9d1280d1732a8e517c0b51606db51113dcd1b2dc
Thomas Grainger <tom@procensus.com>
2017-03-01 13:38:02 +0000

    add fixer, Fixes #46

--- src/rules/prefer-object-spread.js
+++ src/rules/prefer-object-spread.js
@@ -1,25 +1,50 @@
-export default function preferObjectSpreadRule(context) {
-	const cloneOnly = context.options[0] !== 'always';
+const message = 'Use a spread property instead of Object.assign().';
 
-	return {
-		'CallExpression'(node) {
-			const { callee, arguments: args } = node;
+function createFix(node, sourceCode) {
+	function fix(fixer) {
+		const args = node
+			.arguments
+			.map(n => `...(${sourceCode.getText(n)})`)
+			.join();
+		return fixer.replaceText(node, `({${args}})`);
+	}
 
-			const isObjectAssign = callee.type === 'MemberExpression'
-				&& callee.object.name === 'Object'
-				&& callee.property.name === 'assign';
-
-			const isCloneOperation = args.length
-				&& args[0].type === 'ObjectExpression';
-
-			// Object spread can't be used when passing a spread element to Object.assign()
-			// E.g., Object.assign(...a)
-			const hasSpreadElement = args.length
-				&& args.some(x => x.type === 'SpreadElement');
-
-			if (isObjectAssign && !hasSpreadElement && (isCloneOperation || !cloneOnly)) {
-				context.report(node, 'Use a spread property instead of Object.assign().');
-			}
-		},
-	};
+	return fix;
 }
+
+export default {
+	meta: {
+		docs: {
+			description: message,
+			category: 'Best Practices',
+			recommended: false,
+		},
+		fixable: 'code',
+	},
+	create(context) {
+		const cloneOnly = context.options[0] !== 'always';
+		const sourceCode = context.getSourceCode();
+
+		return {
+			'CallExpression'(node) {
+				const { callee, arguments: args } = node;
+
+				const isObjectAssign = callee.type === 'MemberExpression'
+					&& callee.object.name === 'Object'
+					&& callee.property.name === 'assign';
+
+				const isCloneOperation = args.length
+					&& args[0].type === 'ObjectExpression';
+
+				// Object spread can't be used when passing a spread element to Object.assign()
+				// E.g., Object.assign(...a)
+				const hasSpreadElement = args.length
+					&& args.some(x => x.type === 'SpreadElement');
+
+				if (isObjectAssign && !hasSpreadElement && (isCloneOperation || !cloneOnly)) {
+					context.report({ node, message, fix: createFix(node, sourceCode) });
+				}
+			},
+		};
+	},
+};
